2b5f15f93cfbc58963e2f367e84d268202aaa9d1
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-01 08:21:52 +0900

    explicitly destroy the streams
    Details: https://github.com/mafintosh/duplexify

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ var imageSizeStream = require('image-size-stream');
 var wrapPromise = require('wrap-promise');
 
 module.exports = function httpImageSizePromise(url, options) {
+  var proxy;
   var size = imageSizeStream(options);
 
   return wrapPromise(function(resolve, reject) {
     size
-      .on('error', reject)
-      .on('size', resolve);
+      .on('error', function(err) {
+        proxy.destroy();
+        reject(err);
+      })
+      .on('size', function(dimensions) {
+        proxy.destroy();
+        resolve(dimensions);
+      });
 
-    got(url, options)
+    proxy = got(url, options)
       .on('error', reject)
       .pipe(size);
   });
